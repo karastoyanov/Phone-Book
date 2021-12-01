3122561fda593298a6f41f0f7ff91bc1bd7e200d
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-01 22:16:26 +0200

    Last Update: 01/12/2021

--- contact_book_v2.py
+++ contact_book_v2.py
@@ -1,3 +1,6 @@
+import fileinput
+
+
 def action(choice):
     user_input = input("Select one of the following operations:\n[C] - Create a new contact\n[A] - Show all contacts\n[S] - Search for a contact\n[D] - Delete contact\n[X] - Exit\n").upper()
     if user_input == "C":
@@ -25,15 +28,23 @@ def create_contact(new_contact):
     contacts = {}
     contacts[first_name, last_name] = phone_number
     file = open('contacts.txt', 'a')
-    file.write(f"Contact name:\n {first_name}\n {last_name}\nContact phone number:\n {phone_number}\n\n###########################\n\n")
+    file.write(f"Contact name: {first_name} >>> {last_name}. Contact phone number: {phone_number} \n\n###########################\n\n")
     file.close()
     return action(None)
 
 
 def delete_contact(contact):
-    pass
-    
-
-
-
+    contact_name = input("Enter a contact name to delete:\n")
+    with open('contacts.txt', 'r+') as file:
+        lines = file.readlines()
+        for _ in range(len(lines)):
+            current_object = lines[_]
+            if contact_name in current_object:
+                lines.remove(current_object)
+                file.truncate(_)
+                for element in lines:
+                    file.write(element)
+                file.close()
+                break
+                    
 action(None)
