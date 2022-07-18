715f0e31c97099cc50b7b0ae57448908c4e56343
Aleksandar Karastoyanov <86146020+karastoyanov@users.noreply.github.com>
2022-07-18 22:25:05 +0300

    new commit

--- old_files/PhoneBook_v1/create_new_contact.py
+++
@@ -1,8 +0,0 @@
-def create_contact():
-    first_name = input("Enter First Name: ")
-    last_name = input("Enter Last Name: ")
-    email = input("Enter Email: ")
-    phone = input("Enter Phone: ")
-    return first_name, last_name, email, phone
-
-create_contact()
